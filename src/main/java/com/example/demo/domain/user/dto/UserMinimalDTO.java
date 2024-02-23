3b0bc833a9ac2c3e74e615d8e66e63741b19ead5
Jessica Fuerst <jvfuerst01@gmail.com>
2024-02-23 16:21:52 +0100

    updated blog post to user relation

---
+++ src/main/java/com/example/demo/domain/user/dto/UserMinimalDTO.java
@@ -0,0 +1,15 @@
+package com.example.demo.domain.user.dto;
+
+import com.example.demo.core.generic.AbstractDTO;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Accessors(chain = true)
+public class UserMinimalDTO extends AbstractDTO {
+
+}
