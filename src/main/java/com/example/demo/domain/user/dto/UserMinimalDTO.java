1cc8f0b62943194c0c8b0d750d181d22a23f7a20
Emma Bright <113352515+emmabrght@users.noreply.github.com>
2024-02-29 11:09:36 +0100

    Added users name to the user DTO
    So that the author of each blogpost can be displayed in frontend

--- src/main/java/com/example/demo/domain/user/dto/UserMinimalDTO.java
+++ src/main/java/com/example/demo/domain/user/dto/UserMinimalDTO.java
@@ -1,6 +1,7 @@
 package com.example.demo.domain.user.dto;
 
 import com.example.demo.core.generic.AbstractDTO;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,4 +17,10 @@ public class UserMinimalDTO extends AbstractDTO {
     public UserMinimalDTO(UUID id){
         super(id);
     }
+
+    @Size(min=1, max=100, message = "has to be between 1 and 100 characters")
+    private String firstName;
+
+    @Size(min=1, max=100, message = "has to be between 1 and 100 characters")
+    private String lastName;
 }
